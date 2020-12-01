412b3525255e95d3e230a7513d19598270006758
Axel Kohlmeyer <akohlmey@gmail.com>
2020-11-30 20:35:11 -0500

    refactor angle constraint fix to check all 1-2 neighbors and accept if any matches

--- src/MC/fix_bond_create_angle.cpp
+++ src/MC/fix_bond_create_angle.cpp
@@ -36,107 +36,131 @@ int FixBondCreateAngle::constrain(int i, int j, double amin, double amax)
   double **x = atom->x;
   int **nspecial = atom->nspecial;
   tagint **special = atom->special;
-
-  double v1x = 0.0;
-  double v1y = 0.0;
-  double v1z = 0.0;
-  double v2x = 0.0;
-  double v2y = 0.0;
-  double v2z = 0.0;
-
+  double v1x,v1y,v1z,v2x,v2y,v2z;
   double angle1,angle2;
 
-  int flag = 0;
+  // pass if both atoms have no 1-2 neighbors: bond is always formed
 
-  // pass if both atoms have no neighbors: bond is always formed
-
-  if (nspecial[i][0] == 0 && nspecial[j][0] == 0) flag = 1;
+  if (nspecial[i][0] == 0 && nspecial[j][0] == 0)
+    return 1;
 
   // pass if i has at least one neighbor and angle constraint is met
 
-  if (nspecial[i][0] != 0 && nspecial[j][0] == 0) {
+  if ((nspecial[i][0] > 0) && (nspecial[j][0] == 0)) {
 
     // calculate first vector between i and j
-    // calculate second vector between i and its first neighbor
+    // calculate second vector between i and its 1-2 neighbors
 
-    v1x = x[i][0] - x[j][0];
-    v1y = x[i][1] - x[j][1];
-    v1z = x[i][2] - x[j][2];
-    v2x = x[i][0] - x[atom->map(special[i][0])][0];
-    v2y = x[i][1] - x[atom->map(special[i][0])][1];
-    v2z = x[i][2] - x[atom->map(special[i][0])][2];
+    for (int k = 0; k < nspecial[i][0]; ++k) {
 
-    // calculate angle between both vectors
-    // set flag if the angle constraint is met
+      int mapk = atom->map(special[i][k]);
+      if ((mapk < 0) || (mapk == j)) continue;
 
-    angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z)/
-        (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
-    if (amin <= angle1 && angle1 <= amax) flag = 1;
+      v1x = x[i][0] - x[j][0];
+      v1y = x[i][1] - x[j][1];
+      v1z = x[i][2] - x[j][2];
+      v2x = x[i][0] - x[mapk][0];
+      v2y = x[i][1] - x[mapk][1];
+      v2z = x[i][2] - x[mapk][2];
+
+      // calculate angle between both vectors
+      // set flag if the angle constraint is met
+
+      angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z)/
+          (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
+      if ((amin <= angle1) && (angle1 <= amax))
+        return 1;
+    }
   }
 
   // pass if j has at least one neighbor and angle constraint is met
 
-  if (nspecial[j][0] != 0 && nspecial[i][0] == 0) {
+  if ((nspecial[j][0] > 0) && (nspecial[i][0] == 0)) {
 
     // calculate first vector between j and i
-    // calculate second vector between j and its first neighbor
+    // calculate second vector between j and its 1-2 neighbors
 
-    v1x = x[j][0] - x[i][0];
-    v1y = x[j][1] - x[i][1];
-    v1z = x[j][2] - x[i][2];
-    v2x = x[j][0] - x[atom->map(special[j][0])][0];
-    v2y = x[j][1] - x[atom->map(special[j][0])][1];
-    v2z = x[j][2] - x[atom->map(special[j][0])][2];
+    for (int k = 0; k < nspecial[j][0]; ++k) {
 
-    // calculate angle between both vectors
-    // set flag if angle constraint is met
+      int mapk = atom->map(special[j][k]);
+      if ((mapk < 0) || (mapk == i)) continue;
 
-    angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
-        (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
-    if (amin <= angle1 && angle1 <= amax) flag = 1;
+      v1x = x[j][0] - x[i][0];
+      v1y = x[j][1] - x[i][1];
+      v1z = x[j][2] - x[i][2];
+      v2x = x[j][0] - x[mapk][0];
+      v2y = x[j][1] - x[mapk][1];
+      v2z = x[j][2] - x[mapk][2];
+
+      // calculate angle between both vectors
+      // set flag if angle constraint is met
+
+      angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
+          (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
+      if ((amin <= angle1) && (angle1 <= amax))
+        return 1;
+    }
   }
 
   // pass if both i and j have at least one neighbor and angle constraint is met
 
-  if (nspecial[i][0] != 0 && nspecial[j][0] != 0) {
+  if ((nspecial[i][0] > 0) && (nspecial[j][0] > 0)) {
 
     // Calculate 1st angle
     // calculate first vector between i and j
-    // calculate second vector between i and its first neighbor
+    // calculate second vector between i and its 1-2 neighbors
 
-    v1x = x[i][0] - x[j][0];
-    v1y = x[i][1] - x[j][1];
-    v1z = x[i][2] - x[j][2];
-    v2x = x[i][0] - x[atom->map(special[i][0])][0];
-    v2y = x[i][1] - x[atom->map(special[i][0])][1];
-    v2z = x[i][2] - x[atom->map(special[i][0])][2];
+    for (int k = 0; k < nspecial[i][0]; ++k) {
 
-    // calculate angle between both vectors
+      int mapk = atom->map(special[i][k]);
+      if ((mapk < 0) || (mapk == j)) continue;
 
-    angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
-        (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
+      v1x = x[i][0] - x[j][0];
+      v1y = x[i][1] - x[j][1];
+      v1z = x[i][2] - x[j][2];
+      v2x = x[i][0] - x[mapk][0];
+      v2y = x[i][1] - x[mapk][1];
+      v2z = x[i][2] - x[mapk][2];
 
-    // Calculate 2nd angle
-    // calculate first vector between i and j
-    // calculate second vector between i and its first neighbor
+      // calculate angle between both vectors
 
-    v1x = x[j][0] - x[i][0];
-    v1y = x[j][1] - x[i][1];
-    v1z = x[j][2] - x[i][2];
-    v2x = x[j][0] - x[atom->map(special[j][0])][0];
-    v2y = x[j][1] - x[atom->map(special[j][0])][1];
-    v2z = x[j][2] - x[atom->map(special[j][0])][2];
+      angle1 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
+          (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
 
-    // calculate angle between both vectors
+      // skip if first angle constraint is not met
 
-    angle2 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
-        (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
+      if ((angle1 < amin) || (angle1 > amax)) continue;
 
-    // set flag if the angle constraint is met
+      // Calculate 2nd angle
+      // calculate first vector between i and j
+      // calculate second vector between j and its 1-2 neighbors
 
-    if ((amin <= angle1 && angle1 <= amax) && (amin <= angle2 && angle2 <= amax))
-      flag = 1;
+      for (int l = 0; l < nspecial[j][0]; ++l) {
+
+        int mapl = atom->map(special[j][l]);
+        if ((mapl < 0) || (mapl == i)) continue;
+
+        // calculate second vector between i and its 1-2 neighbors
+
+        v1x = x[j][0] - x[i][0];
+        v1y = x[j][1] - x[i][1];
+        v1z = x[j][2] - x[i][2];
+        v2x = x[j][0] - x[mapl][0];
+        v2y = x[j][1] - x[mapl][1];
+        v2z = x[j][2] - x[mapl][2];
+
+        // calculate angle between both vectors
+
+        angle2 = acos((v1x*v2x + v1y*v2y + v1z*v2z) /
+            (sqrt(v1x*v1x + v1y*v1y + v1z*v1z)*sqrt(v2x*v2x + v2y*v2y + v2z*v2z)));
+
+        // set flag if the second angle constraint is also met
+
+        if ((amin <= angle2) && (angle2 <= amax))
+          return 1;
+      }
+    }
   }
 
-  return flag;
+  return 0;
 }
